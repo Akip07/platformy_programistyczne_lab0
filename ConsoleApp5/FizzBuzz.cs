95a6315cdbc46476bf9da07b46a561882057496b
Akip <grzywek123@gmail.com>
2024-02-28 17:46:01 +0100

    Add project files.

---
+++ ConsoleApp5/FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class FizzBuzz
+    {
+        int upperLimit = 10;
+
+        public void Check()
+        {
+
+            for(int i = 0; i<=upperLimit; i++)
+            {
+                bool byFive = false;
+                bool byThree = false;
+                //Console.Write(i);
+                if(i%3 == 0)
+                {
+                    Console.Write("Fizz");
+                    byThree = true;
+                }
+                if(i%5 == 0)
+                {
+                    Console.Write("Buzz");
+                    byFive = true;
+                }
+                if(!byFive && !byThree)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+
+
+            }
+
+        }
+        public FizzBuzz(int _upperLimit)
+        {
+            this.upperLimit = _upperLimit;
+        }
+
+    }
+}
